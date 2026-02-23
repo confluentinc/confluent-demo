280ab47c4880d0ec36642c400623896db8b77669
Justin Lee <jlee@confluent.io>
2026-02-23 22:28:38 +0800

    Minor

--- precheck.sh
+++ precheck.sh
@@ -186,7 +186,7 @@ export BASE_IP
 # Super simple validation
 if [[ "$BASE_IP" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
     echo "Using IP ${BASE_IP}"
-    export BASE_DOMAIN="$(echo ${BASE_IP} | tr '.' '-').nip.io" 
+    export BASE_DOMAIN="$(echo ${BASE_IP} | tr '.' '-').nip.io"
     export BASE_INT_DOMAIN="int.${BASE_DOMAIN}"
     export BASE_EXT_DOMAIN="ext.${BASE_DOMAIN}"
     echo "Setting BASE_DOMAIN in ./.env to ${BASE_DOMAIN}"
